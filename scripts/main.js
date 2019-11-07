ffbe7dce1b27f24137837c3ed748f14df5f1061d
Kippiii <ianorzel@gmail.com>
2019-11-07 14:36:21 -0500

    Merging Suggestions
    Adding commits from Kippi's Pull Request:
    
    * Making background generation random
    
    * Fixing the image and the navbar and updating Bootstrap
    
    * Removing space at top
    
    * Continually improving navbar
    
    * Making things look visually nicer
    
    * Fixing stuff
    
    * Continuing to work on navbar
    
    * Continually fixing the navbar

--- scripts/main.js
+++ scripts/main.js
@@ -8,3 +8,53 @@ for (var i = 0; i < x.length; i++) {
         x[i].className = "active";
     }
 }
+
+$(window).on('load resize', function() {
+    if(this.matchMedia('(min-width: 992px)').matches) {
+        $('.dropdown').hover(
+            function() {
+                $(this).addClass('show')
+                $(this).find($('.dropdown-toggle')).attr('aria-expanded', 'true')
+                $menu = $(this).find($('.dropdown-menu'))
+                $menu.stop()
+                $menu.addClass('show')
+                $menu.hide()
+                $menu.slideDown(250)
+            }, function() {
+                $(this).removeClass('show')
+                $(this).find($('.dropdown-toggle')).attr('aria-expanded', 'false')
+                $(this).find($('.dropdown-menu')).stop()
+                $(this).find($('.dropdown-menu')).slideUp(250, function() {
+                    $(this).removeClass('show')
+                })
+            }
+        )
+    }else{
+        //$('.dropdown').unbind('mouseenter mouseleave')
+    }
+})
+
+$(document).on('scroll', function() {
+    $image = $('.nav-image')
+    max_image_height = 50
+    min_image_height = 0
+    if($(document).scrollTop() > 50) {
+        if($image.is(':animated')) {
+            $image.stop()
+        }
+        if($image.height() > min_image_height) {
+            $image.animate({
+                height: min_image_height
+            }, 100)
+        }
+    }else{
+        if($image.is(':animated')) {
+            $image.stop()
+        }
+        if($image.height() < max_image_height) {
+            $image.animate({
+                height: max_image_height
+            }, 100)
+        }
+    }
+})
