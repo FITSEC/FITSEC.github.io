77b293daeb49731f5876a2ac7cd6f115892f1577
Nick Cottrell <nrc1mr.e3@gmail.com>
2019-12-17 13:53:55 -0500

    Changed binary to hex based off pages name

--- scripts/backgroundGen.js
+++ scripts/backgroundGen.js
@@ -1,31 +1,51 @@
 // JavaScript Document
-/*$(document).ready(function(){
-	var randomizer = document.getElementById("backgroundNum").value;
-	
-});*/
-function toBinary(inDec) {
-    var bin = [];
+function toHex(inDec) {
+    var result = "";
     while (inDec > 0) {
-        bin.push(inDec % 2);
-        inDec = (inDec - (inDec % 2)) / 2;
+        switch (inDec % 16) {
+            case 10:
+                result = "A" + result;
+                break;
+            case 11:
+                result = "B" + result;
+                break;
+            case 12:
+                result = "C" + result;
+                break;
+            case 13:
+                result = "D" + result;
+                break;
+            case 14:
+                result = "E" + result;
+                break;
+            case 15:
+                result = "F" + result;
+            default:
+                result = (inDec % 16) + result;
+                break;
+        }
+        inDec = (inDec - (inDec % 16)) / 16;
     }
-    bin.reverse();
-    return bin.join("");
+    return result;
+}
+
+function genHex() {
+    var file = location.pathname.split("/").slice(-1)[0].split(".")[0];
+    var result = "";
+    for (var i = 0; i < file.length; i++) {
+        result += toHex(file.substring(i, i + 1).charCodeAt()) + " ";
+    }
+    return result + "20 ";
 }
 
 function genBackground() {
-    var r = Math.floor(Math.random() * 1000000000000);
-    var randomize = toBinary(r).toString().split("");
+    var key = genHex();
     var set = "";
-    for (i = 0; i < randomize.length; i++) {
-        set += randomize[i] + " ";
+    for (i = 0; i < 10; i++) {
+        set += key;
     }
-    console.log(set);
     document.getElementById("bgCode").innerHTML = set;
     for (i = 0; i < 1000; i++) {
         document.getElementById("bgCode").innerHTML += set;
     }
-    //$("#background").append(randomize);
-    //$("#background").html("<p>1111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111</p>");
-
 }
