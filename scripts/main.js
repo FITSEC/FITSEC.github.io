a645b327c1c6536b4ad28baf70a4640f01e01314
Nick Cottrell <nrc1mr.e3@gmail.com>
2019-12-17 21:14:28 -0500

    Reformatted files

--- scripts/main.js
+++ scripts/main.js
@@ -13,7 +13,7 @@ for (var i = 0; i < x.length; i++) {
 */
 
 $(window).on('load resize', function() {
-    if(this.matchMedia('(min-width: 992px)').matches) {
+    if (this.matchMedia('(min-width: 992px)').matches) {
         $('.dropdown').hover(
             function() {
                 $(this).addClass('show')
@@ -23,7 +23,8 @@ $(window).on('load resize', function() {
                 $menu.addClass('show')
                 $menu.hide()
                 $menu.slideDown(250)
-            }, function() {
+            },
+            function() {
                 $(this).removeClass('show')
                 $(this).find($('.dropdown-toggle')).attr('aria-expanded', 'false')
                 $(this).find($('.dropdown-menu')).stop()
@@ -32,7 +33,7 @@ $(window).on('load resize', function() {
                 })
             }
         )
-    }else{
+    } else {
         //$('.dropdown').unbind('mouseenter mouseleave')
     }
 })
@@ -41,20 +42,20 @@ $(document).on('scroll', function() {
     $image = $('.nav-image')
     max_image_height = 50
     min_image_height = 0
-    if($(document).scrollTop() > 50) {
-        if($image.is(':animated')) {
+    if ($(document).scrollTop() > 50) {
+        if ($image.is(':animated')) {
             $image.stop()
         }
-        if($image.height() > min_image_height) {
+        if ($image.height() > min_image_height) {
             $image.animate({
                 height: min_image_height
             }, 100)
         }
-    }else{
-        if($image.is(':animated')) {
+    } else {
+        if ($image.is(':animated')) {
             $image.stop()
         }
-        if($image.height() < max_image_height) {
+        if ($image.height() < max_image_height) {
             $image.animate({
                 height: max_image_height
             }, 100)
